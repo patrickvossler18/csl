9d76681334a7f676d0a7d99222d3b24195236ba5
Patrick Vossler <patrick.vossler18@gmail.com>
2023-02-27 10:32:54 -0800

    add setup.py so we can install the package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="csl",
+    version="0.1.0",
+    author="Luiz Chamon",
+    author_email="luiz.chamon@simtech.uni-stuttgart.de",
+    packages=["csl"],
+    url="http://pypi.python.org/pypi/PackageName/",
+    license="LICENSE.txt",
+    description="A Python package based around pytorch to simplify the definition of constrained learning problems and then solving them.",
+    long_description=open("README.md").read(),
+    install_requires=[],
+)
